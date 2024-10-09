a0707b66bc8dd19e8121ca3b8f0c6aef5bec3c32
Ahmed Saqr <ah8081189@gmail.com>
2024-10-09 18:34:15 +0300

    update the controller

--- My App/controllers/GetPredictions.py
+++ My App/controllers/GetPredictions.py
@@ -1,25 +1,12 @@
+import numpy as np
 
-def GetPredictions(model, encoder, inputs: list):
+def GetPredictions(model, encoder,inputs: list):
     result = {
         0: "Not Rainy",
         1: "Rainy",
     }
 
-    inputs[1] = encoder.transform([inputs[1]])
-
-    Temp_Humidity_Interaction = inputs[2] * inputs[3]
-    Wind_Cloud_Ratio = inputs[4] / inputs[6]
-    Year = inputs[0].dt.year
-    Month = inputs[0].dt.month
-    Day = inputs[0].dt.day
-
-    inputs.append(Temp_Humidity_Interaction)
-    inputs.append(Wind_Cloud_Ratio)
-    inputs.append(Year)
-    inputs.append(Month)
-    inputs.append(Day)
-
-    del inputs[0]
+    inputs[0] = encoder.transform([inputs[0]])[0]
 
     predictions = model.predict([inputs])[0]
 
